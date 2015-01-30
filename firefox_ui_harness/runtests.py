bf544f9e815dcb0601525867a6356ade9d2ef12c
Barbara Miller <galgeek@me.com>
2015-01-30 09:11:58 -0800

    move default prefs to import file

--- firefox_ui_harness/runtests.py
+++ firefox_ui_harness/runtests.py
@@ -11,61 +11,19 @@ from marionette.runtests import cli
 import firefox_ui_tests
 
 from .arguments import ReleaseTestParser
+from .default_prefs import default_prefs
 from .testcase import FirefoxTestCase
 
 
 class ReleaseTestRunner(BaseMarionetteTestRunner):
-    extra_prefs = {
-        'browser.tabs.remote.autostart': False,
-        'app.update.auto': False,
-        'app.update.enabled': False,
-        'browser.dom.window.dump.enabled': True,
-        'browser.newtab.url': 'about:blank',
-        'browser.newtabpage.enabled': False,
-        'browser.safebrowsing.enabled': False,
-        'browser.safebrowsing.malware.enabled': False,
-        'browser.search.update': False,
-        'browser.sessionstore.resume_from_crash': False,
-        'browser.shell.checkDefaultBrowser': False,
-        'browser.startup.page': 0,
-        'browser.tabs.animate': False,
-        'browser.tabs.warnOnClose': False,
-        'browser.tabs.warnOnOpen': False,
-        'browser.uitour.enabled': False,
-        'browser.warnOnQuit': False,
-        'datareporting.healthreport.service.enabled': False,
-        'datareporting.healthreport.uploadEnabled': False,
-        'datareporting.healthreport.documentServerURI': "http://%(server)s/healthreport/",
-        'datareporting.healthreport.about.reportUrl': "http://%(server)s/abouthealthreport/",
-        'datareporting.policy.dataSubmissionEnabled': False,
-        'datareporting.policy.dataSubmissionPolicyAccepted': False,
-        'dom.ipc.reportProcessHangs': False,
-        'dom.report_all_js_exceptions': True,
-        'extensions.enabledScopes': 5,
-        'extensions.autoDisableScopes': 10,
-        'extensions.getAddons.cache.enabled': False,
-        'extensions.installDistroAddons': False,
-        'extensions.logging.enabled': True,
-        'extensions.showMismatchUI': False,
-        'extensions.update.enabled': False,
-        'extensions.update.notifyUser': False,
-        'geo.provider.testing': True,
-        'javascript.options.showInConsole': True,
-        'security.notification_enable_delay': 0,
-        'signon.rememberSignons': False,
-        'startup.homepage_welcome_url': 'about:blank',
-        'toolkit.startup.max_resumed_crashes': -1,
-        'toolkit.telemetry.enabled': False,
-    }
-
     def __init__(self, *args, **kwargs):
         if not kwargs.get('server_root'):
             kwargs['server_root'] = firefox_ui_tests.resources
 
         prefs = kwargs.get('prefs', {})
-        extra_prefs = copy.deepcopy(ReleaseTestRunner.extra_prefs)
-        extra_prefs.update(prefs)
-        kwargs['prefs'] = extra_prefs
+        runner_prefs = copy.deepcopy(default_prefs)
+        runner_prefs.update(prefs)
+        kwargs['prefs'] = runner_prefs
 
         BaseMarionetteTestRunner.__init__(self, *args, **kwargs)
         self.test_handlers = [FirefoxTestCase]
