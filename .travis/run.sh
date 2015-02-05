068c57f9e8bc1f1144db22736fd35d853b3a8718
Barbara Miller <galgeek@me.com>
2015-02-05 10:58:29 -0800

    Updates per 5Feb feedback

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -e
-set -x
-if [[ "$(uname -s)" == "Darwin" ]]; then
+set -ev
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     firefox-ui-tests --binary /Applications/FirefoxNightly.app/Contents/MacOS/firefox
 else
     firefox-ui-tests --binary firefox/firefox
