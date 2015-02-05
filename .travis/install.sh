775db0ac26e7d2bf7d3d492d92b65e9c2d058ab2
Barbara Miller <galgeek@me.com>
2015-02-05 10:58:29 -0800

    Updates per 5Feb feedback

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,42 +1,27 @@
 #!/bin/bash
 
-set -e
-set -x
+set -ev
 
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    DARWIN=true
-else
-    DARWIN=false
-fi
-
-if [[ "$DARWIN" = true ]]; then
-    brew update
-
-    # xvfb for OS X?
-
-    # flash via command line for OS X?
-
-else
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     sudo apt-get update
+
+    export DISPLAY=:99
+    export MOZ_XVFB=1
     /sbin/start-stop-daemon --start --quiet --make-pidfile --pidfile /tmp/custom_xvfb_99.pid \
          --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1024x768x24
-    sudo apt-get install flashplugin-nonfree subversion
 fi
 
-# As long as we have invasive changes lets get the trunk version of marionette-client
+# As long as we have invasive changes, let's get the trunk version of marionette-client
 svn checkout https://github.com/mozilla/gecko-dev/trunk/testing/marionette/client
 cd client && sudo python setup.py develop && cd ..
 
 sudo python setup.py develop
 sudo pip install mozdownload pep8
 
-# Run pep8 on all except the checked out marionette-client folder
-pep8 --max-line-length=99 --exclude=client .
-
 # Download Firefox Nightly which is compatible with the Marionette client version used 
 mozdownload -t daily
 
-if [[ "$DARWIN" = true ]]; then
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     hdiutil mount *firefox*.mac.dmg
     sudo cp -R /Volumes/Nightly/FirefoxNightly.app /Applications
     cd ~
