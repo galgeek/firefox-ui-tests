ea732f1aa9a9dcf36fff9592728829312a3b8bfc
Barbara Miller <galgeek@me.com>
2015-02-10 11:32:51 -0800

    Update per 10Feb feedback

--- firefox_ui_tests/functional/locationbar/test_escape_autocomplete.py
+++ firefox_ui_tests/functional/locationbar/test_escape_autocomplete.py
@@ -8,21 +8,12 @@ from firefox_ui_harness.testcase import FirefoxTestCase
 
 class TestEscapeAutocomplete(FirefoxTestCase):
 
-    def tearDown(self):
-        autocompleteresults = self.browser.navbar.locationbar.autocomplete_results
+    def setUp(self):
+        FirefoxTestCase.setUp(self)
 
-        if autocompleteresults.is_open:
-            autocompleteresults.close(force=True)
-            self.wait_for_condition(not autocompleteresults.is_open)
-
-        FirefoxTestCase.tearDown(self)
-
-    @skip_under_xvfb
-    def test_escape_autocomplete(self):
         # Clear complete history so there's no interference from previous entries.
         self.places.remove_all_history()
 
-        # Open some local pages to set up the test environment.
         self.test_urls = [
             'layout/mozilla.html',
             'layout/mozilla_community.html',
@@ -30,29 +21,36 @@ class TestEscapeAutocomplete(FirefoxTestCase):
         self.test_urls = [self.marionette.absolute_url(t)
                           for t in self.test_urls]
 
+        self.test_string = 'mozilla'
+
+        self.locationbar = self.browser.navbar.locationbar
+        self.autocompleteresults = self.locationbar.autocomplete_results
+
+    def tearDown(self):
+        self.autocompleteresults.close(force=True)
+        FirefoxTestCase.tearDown(self)
+
+    @skip_under_xvfb
+    def test_escape_autocomplete(self):
+
+        # Open some local pages
         def load_urls():
             with self.marionette.using_context('content'):
                 for url in self.test_urls:
                     self.marionette.navigate(url)
         self.places.wait_for_visited(self.test_urls, load_urls)
 
-        # Set variables
-        TEST_STRING = 'mozilla'
-
-        locationbar = self.browser.navbar.locationbar
-        autocompleteresults = locationbar.autocomplete_results
-
         # Clear the location bar, type the test string, check that autocomplete list opens
-        locationbar.clear()
-        locationbar.urlbar.send_keys(TEST_STRING)
-        self.assertEqual(locationbar.value, TEST_STRING)
-        self.wait_for_condition(lambda _: autocompleteresults.is_open)
+        self.locationbar.clear()
+        self.locationbar.urlbar.send_keys(self.test_string)
+        self.assertEqual(self.locationbar.value, self.test_string)
+        self.wait_for_condition(lambda _: self.autocompleteresults.is_open)
 
         # Press escape, check location bar value, check autocomplete list closed
-        locationbar.urlbar.send_keys(self.keys.ESCAPE)
-        self.assertEqual(locationbar.value, TEST_STRING)
-        self.assertFalse(autocompleteresults.is_open)
+        self.locationbar.urlbar.send_keys(self.keys.ESCAPE)
+        self.assertEqual(self.locationbar.value, self.test_string)
+        self.wait_for_condition(lambda _: not self.autocompleteresults.is_open)
 
         # Press escape again and check that locationbar returns to the page url
-        locationbar.urlbar.send_keys(self.keys.ESCAPE)
-        self.assertEqual(locationbar.value, self.test_urls[-1])
+        self.locationbar.urlbar.send_keys(self.keys.ESCAPE)
+        self.assertEqual(self.locationbar.value, self.test_urls[-1])
