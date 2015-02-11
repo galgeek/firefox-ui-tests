9988cebc64aee07b4064fddaf74593ad0b44bfe0
Barbara Miller <galgeek@me.com>
2015-02-11 12:17:16 -0800

    Update per unaddressed feedback

--- firefox_ui_tests/functional/locationbar/test_access_location_bar.py
+++ firefox_ui_tests/functional/locationbar/test_access_location_bar.py
@@ -2,8 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette import By
-
 from firefox_ui_harness.decorators import skip_under_xvfb
 from firefox_ui_harness.testcase import FirefoxTestCase
 
@@ -12,62 +10,56 @@ class TestAccessLocationBar(FirefoxTestCase):
 
     def setUp(self):
         FirefoxTestCase.setUp(self)
-        self.location_bar = self.browser.navbar.locationbar
-        self.url_bar = self.location_bar.urlbar
 
-    @skip_under_xvfb
-    def test_access_location_bar_history(self):
-        # Purge history
+        # Clear complete history so there's no interference from previous entries.
         self.places.remove_all_history()
 
-        # Navigate to several urls in order to populate history
         self.test_urls = [
             'layout/mozilla_projects.html',
             'layout/mozilla.html',
-            'layout/mozilla_mission.html']
+            'layout/mozilla_mission.html'
+        ]
         self.test_urls = [self.marionette.absolute_url(t)
                           for t in self.test_urls]
+        self.test_urls.append('about:blank')
 
-        for url in self.test_urls:
-            self.location_bar.load_url(url)
+        self.locationbar = self.browser.navbar.locationbar
+        self.autocomplete_results = self.locationbar.autocomplete_results
+        self.urlbar = self.locationbar.urlbar
 
-        with self.marionette.using_context('content'):
-            self.marionette.navigate('about:blank')
+    @skip_under_xvfb
+    def test_access_locationbar_history(self):
+
+        # Open some local pages, then about:blank
+        def load_urls():
+            with self.marionette.using_context('content'):
+                for url in self.test_urls:
+                    self.marionette.navigate(url)
+        self.places.wait_for_visited(self.test_urls, load_urls)
+
         # Need to blur url bar or autocomplete won't load - bug 1038614
-        self.marionette.execute_script("""arguments[0].blur();""", script_args=[self.url_bar])
+        self.marionette.execute_script("""arguments[0].blur();""", script_args=[self.urlbar])
 
-        # Clear contents of url bar to focus
-        self.location_bar.clear()
+        # Clear contents of url bar to focus, then arrow down for list of visited sites
+        # Verify that autocomplete is open and results are displayed
+        self.locationbar.clear()
+        self.urlbar.send_keys(self.keys.ARROW_DOWN)
+        self.wait_for_condition(lambda _: self.autocomplete_results.is_open)
+        self.wait_for_condition(lambda _: len(self.autocomplete_results.visible_results) > 1)
 
-        # Arrow down to navigate list of visited sites.
-        # They should appear in this order:
+        # List of visited sites should appear in this order:
         # layout/mozilla_mission.html
         # layout/mozilla.html
         # layout/mozilla_projects.html
-        self.url_bar.send_keys(self.keys.ARROW_DOWN)
-
-        # Verify that autocomplete is open
-        self.wait_for_condition(lambda _: self.location_bar.autocomplete_results.is_open)
-
-        # Verify that results are displayed in autocomplete
-        self.results_length = len(self.location_bar.autocomplete_results.visible_results)
-        self.wait_for_condition(lambda _: self.results_length > 1)
 
         # Arrow down again to select first item in list
-        self.url_bar.send_keys(self.keys.ARROW_DOWN)
-
-        # Verify that first item in list is selected
-        def auto_complete_selected_item(mn):
-            # TODO: update to 'visible_results' when 'selected_index' property exists
-            self.results = self.location_bar.autocomplete_results.results
-            return self.results.get_attribute('selectedIndex') == '0'
-        self.wait_for_condition(auto_complete_selected_item)
-
-        # Verify that selected item populates location bar with url
-        self.assertIn('mozilla_mission', self.location_bar.value)
+        # Verify first item is selected and selected item populates location bar with url
+        self.urlbar.send_keys(self.keys.ARROW_DOWN)
+        self.wait_for_condition(lambda _: self.autocomplete_results.selected_index == '0')
+        self.assertIn('mozilla_mission', self.locationbar.value)
 
         # Navigate to the currently selected url
-        self.url_bar.send_keys(self.keys.ENTER)
+        self.urlbar.send_keys(self.keys.ENTER)
 
         # Verify that selected url is loaded by verifying the title of the page
         def check_title(mn):
