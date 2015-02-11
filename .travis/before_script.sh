e204b715007669b0f62ccf78454f69d4c6606481
Barbara Miller <galgeek@me.com>
2015-02-10 19:30:28 -0800

    set env vars per asarih/irc

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -6,14 +6,10 @@ set -ev
 mozdownload -t daily
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-    # hdiutil mount *firefox*.mac.dmg - works!
-    #from http://mxr.mozilla.org/comm-central/source/mozilla/testing/mozbase/mozinstall/mozinstall/mozinstall.py#240 ...
     hdiutil attach -nobrowse -noautoopen *firefox*.mac.dmg
     cp -R /Volumes/Nightly/FirefoxNightly.app /Applications
     cd ~
     hdiutil unmount /Volumes/Nightly/
-    env BINARY=/Applications/FirefoxNightly.app/Contents/MacOS/firefox
 else
     tar -xvf *firefox*.tar.bz2
-    env BINARY=firefox/firefox
 fi
