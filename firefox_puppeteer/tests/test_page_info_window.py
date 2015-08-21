5364f8111072fc631d9954d2f6b41132dec9e43b
Barbara Miller <galgeek@me.com>
2015-08-20 18:56:00 -0700

    remove unittest skip from test_page_info_window

--- firefox_puppeteer/tests/test_page_info_window.py
+++ firefox_puppeteer/tests/test_page_info_window.py
@@ -2,13 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import unittest
-
 from marionette_driver import By
 from firefox_ui_harness import FirefoxTestCase
 
 
-@unittest.skip('Bug 1196006 - test_about_window.py and test_page_info_window.py time out...')
 class TestPageInfoWindow(FirefoxTestCase):
 
     def tearDown(self):
