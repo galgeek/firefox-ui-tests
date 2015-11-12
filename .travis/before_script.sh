cd3bcab6b5275c6ed5bc1b33c203a054b9173348
Barbara Miller <galgeek@me.com>
2015-11-11 23:49:53 -0800

    update .travis/before_script.sh for mozilla-aurora

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -3,7 +3,7 @@ set -ev
 
 if [[ $LOCALE = 'en-US' ]]
 then
-    mozdownload --type daily --branch mozilla-central
+    mozdownload --type daily --branch mozilla-aurora
 else
-    mozdownload --type daily --branch mozilla-central --locale ru
+    mozdownload --type daily --branch mozilla-aurora --locale ru
 fi
